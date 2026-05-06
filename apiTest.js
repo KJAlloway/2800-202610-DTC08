0a3802d305f4d383b921e5c82b665d9dcd9de068
FinnWylie <findlaywylie@gmail.com>
2026-05-05 18:07:21 -0700

    Begin diagnosing error with mongodb imports

--- apiTest.js
+++ apiTest.js
@@ -1,4 +1,15 @@
-const express = require("express");
+// Source - https://stackoverflow.com/a/50052194
+// Posted by GOTO 0, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-05-05, License - CC BY-SA 4.0
+
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// end of copied code
+
+
+import express from "express"
 const app = express();
 const PORT = 3000;
 
@@ -12,4 +23,6 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + '/apiTest.html')
 });
 
-app.get("/")
+app.get("/dbTest.js", (req, res) => {
+    res.sendFile(__dirname + '/dbTest.js')
+})
