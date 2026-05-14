5fbdb08f20c1411519421b92e54e8ddad647b6ac
FinnWylie <findlaywylie@gmail.com>
2026-05-13 18:03:35 -0700

    Test overpass API

---
+++ overpassApiTest/overpassTest.js
@@ -0,0 +1,18 @@
+let name = "Loblaws"
+
+let query = `
+    [out:json];
+area[name="Vancouver"]->.searchArea;
+node["name"="${name}"](area.searchArea);
+out body;
+    `;
+let result = await fetch(
+    "https://overpass.private.coffee/api/interpreter",
+    {
+        headers: {"User-Agent": "CabbagePatch/0.3 (fwylie@my.bcit.ca"},
+        method: "POST",
+        body: "data=" + encodeURIComponent(query),
+    }
+).then((data) => data.json());
+
+console.log(JSON.stringify(result, null, 2));
