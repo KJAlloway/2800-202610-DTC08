40444bae153e5c8c2914fdd4e77a96bfb56526bb
FinnWylie <findlaywylie@gmail.com>
2026-05-15 14:25:48 -0700

    Update test router name

--- backend/routes/routesTest.js
+++ backend/routes/routesTest.js
@@ -2,17 +2,17 @@ import express from "express";
 import db from "../db/connection.js";
 
 // Creates an instance of the Express router, used to define our routes
-const router = express.Router();
+const testRouter = express.Router();
 
 // Gets a list of all the items
-router.get("/", async (req, res) => {
+testRouter.get("/", async (req, res) => {
     let collection = await db.collection("testItems");
     let results = await collection.find({}).toArray();
     res.send(results).status(200);
 });
 
 // Lists restaurants that match the query filter
-router.get("/browse", async (req, res) => {
+testRouter.get("/browse", async (req, res) => {
     try {
         let collection = await db.collection("testItems");
         let query = {
@@ -26,4 +26,4 @@ router.get("/browse", async (req, res) => {
     }
 });
 
-export default router;
+export default testRouter;
